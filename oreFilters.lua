7749e7e8fb5ca902aab3b87879aea808fa3847da
DylanTaylor1 <89085119+DylanTaylor1@users.noreply.github.com>
2023-11-03 17:10:52 -0400

    Add files via upload

---
+++ oreFilters.lua
@@ -0,0 +1,227 @@
+{
+    -- Filter 1: Macerate --> Centrifuge
+    {name='Crushed Copper Ore', filter=1},
+    {name='Crushed Iron Ore', filter=1},
+    {name='Crushed Tin Ore', filter=1},
+    {name='Crushed Gold Ore', filter=1},
+    {name='Crushed Tetrahedrite Ore', filter=1},
+    {name='Crushed Magnetite Ore', filter=1},
+    {name='Crushed Malachite Ore', filter=1},
+    {name='Crushed Diatomite Ore', filter=1},
+    {name='Crushed Deep Iron Ore', filter=1},
+    {name='Crushed Red Zircon Ore', filter=1},
+    {name='Crushed Bismuthinite Ore', filter=1},
+    {name='Crushed Fayalite Ore', filter=1},
+    {name='Crushed Forsterite Ore', filter=1},
+    {name='Crushed Mytryl Ore', filter=1},
+    {name='Crushed Tungstate Ore', filter=1},
+    {name='Crushed Ruby Ore', filter=1},
+    {name='Crushed Chromite Ore', filter=1},
+    {name='Crushed Uraninite Ore', filter=1},
+    {name='Ground Fullers Earth', filter=1},
+    {name='Crushed Spodumene Ore', filter=1},
+    {name='Crushed Pollucite Ore', filter=1},
+    {name='Crushed Rare Earth (I) Ore', filter=1},
+    {name='Crushed Rare Earth (II) Ore', filter=1},
+    {name='Crushed Rare Earth (III) Ore', filter=1},
+    {name='Crushed Thorium Ore', filter=1},
+    {name='Crushed Plutonium 239 Ore', filter=1},
+    {name='Crushed Raw Silicon Ore', filter=1},
+    {name='Crushed Spessartine Ore', filter=1},
+    {name='Crushed Olivine Ore', filter=1},
+    {name='Crushed Calcite Ore', filter=1},
+    {name='Crushed Asbestos Ore', filter=1},
+    {name='Crushed Roquesite Ore', filter=1},
+    {name='Crushed Xenotime Ore', filter=1},
+    {name='Crushed Crocoite Ore', filter=1},
+    {name='Crushed Irarsite Ore', filter=1},
+    {name='Crushed Zirkelite Ore', filter=1},
+    {name='Crushed Hibonite Ore', filter=1},
+    {name='Crushed Cerite Ore', filter=1},
+    {name='Crushed Miessiite Ore', filter=1},
+    {name='Crushed Alburnite Ore', filter=1},
+    {name='Crushed Kashinite Ore', filter=1},
+    {name='Crushed Cryolite (F) Ore', filter=1},
+    {name='Crushed Zirconolite Ore', filter=1},
+    {name='Crushed Lafossaite Ore', filter=1},
+    {name='Crushed Nichromite Ore', filter=1},
+    {name='Crushed Barite (Rd) Ore', filter=1},
+    {name='Crushed Perroudite Ore', filter=1},
+    {name='Crushed Fluorcaphite Ore', filter=1},
+    {name='Crushed Gadolinite (Y) Ore', filter=1},
+    {name='Crushed Gadolinite (Ce) Ore', filter=1},
+    {name='Crushed Geikielite Ore', filter=1},
+    {name='Crushed Titanite Ore', filter=1},
+    {name='Crushed Honeaite Ore', filter=1},
+    {name='Crushed Manganese Ore', filter=1},
+    {name='Crushed Graphite Ore', filter=1}, --X
+    {name='Crushed Mithril Ore', filter=1}, --X
+    {name='Crushed Djurleite Ore', filter=1}, --X
+    {name='Crushed Jasper Ore', filter=1}, --X
+    {name='Crushed Roasted Nickel Ore', filter=1}, --X
+    {name='Crushed Desh Ore', filter=1}, --X
+    {name='Crushed Green Sapphire Ore', filter=1}, --X
+    {name='Crushed Blue Topaz Ore', filter=1}, --X
+    {name='Ground Talc', filter=1}, --X
+    {name='Crushed Perlite Ore', filter=1}, --X
+    
+    -- Filter 2: Wash --> Thermal Centrifuge --> Macerate
+    {name='Crushed Red Descloizite Ore', filter=2},
+    {name='Crushed Orange Descloizite Ore', filter=2},
+    {name='Crushed Apatite Ore', filter=2},
+    {name='Crushed Pyrolusite Ore', filter=2},
+    {name='Crushed Pyrochlore Ore', filter=2},
+    {name='Crushed Palladium Ore', filter=2},
+    {name='Crushed Pentlandite Ore', filter=2},
+    {name='Crushed Chalcopyrite Ore', filter=2},
+    {name='Crushed Coal Ore', filter=2}, --X
+    {name='Crushed Diamond Ore', filter=2}, --X
+    {name='Crushed Titanium Ore', filter=2}, --X
+    {name='Crushed Naquadah Ore', filter=2}, --X
+    {name='Crushed Enriched Naquadah Ore', filter=2}, --X
+    {name='Crushed Sulfur Ore', filter=2}, --X
+    {name='Crushed Ledox Ore', filter=2}, --X
+    {name='Crushed Quantium Ore', filter=2}, --X
+    {name='Crushed Vulcanite Ore', filter=2}, --X
+    {name='Crushed Ceruclase Ore', filter=2}, --X
+    {name='Crushed Infused Gold Ore', filter=2}, --X
+    {name='Crushed Rutile Ore', filter=2}, --X
+    {name='Crushed Aluminium Ore', filter=2}, --X
+    {name='Crushed Ardite Ore', filter=2}, --X
+    {name='Crushed Oriharukon Ore', filter=2}, --X
+    {name='Crushed Orichalcum Ore', filter=2}, --X
+    {name='Crushed Draconium Ore', filter=2}, --X
+    {name='Crushed Vanadium Magnetite Ore', filter=2}, --X
+    {name='Crushed Topaz Ore', filter=2}, --X
+    {name='Crushed Loellingite Ore', filter=2}, --X
+    {name='Crushed Huebnerite Ore', filter=2}, --X
+    {name='Crushed Ferberite Ore', filter=2}, --X
+    {name='Crushed Salt Ore', filter=2}, --X
+    {name='Crushed Rock Salt Ore', filter=2}, --X
+    {name='Crushed Lepidolite Ore', filter=2}, --X
+    {name='Crushed Barite Ore', filter=2}, --X
+    {name='Crushed Kyanite Ore', filter=2}, --X
+    {name='Crushed Beryllium Ore', filter=2}, --X
+    {name='Crushed Jade Ore', filter=2}, --X
+    {name='Crushed Neutronium Ore', filter=2}, --X
+    {name='Crushed Shadow Iron Ore', filter=2}, --X
+    {name='Crushed Grossular Ore', filter=2}, --X
+    {name='Crushed Adamantium Ore', filter=2}, --X
+    {name='Crushed Lithium Ore', filter=2}, --X
+    {name='Crushed Vanadium Ore', filter=2}, --X
+    {name='Crushed Lazurite Ore', filter=2}, --X
+    {name='Crushed Sodalite Ore', filter=2}, --X
+    {name='Ground Kaolinite', filter=2}, --X
+    {name='Crushed Gallium Ore', filter=2}, --X
+    {name='Crushed Niobium Ore', filter=2}, --X
+    {name='Crushed Yttrium Ore', filter=2}, --X
+    {name='Crushed Uranium 235 Ore', filter=2}, --X
+    {name='Crushed Plutonium 241 Ore', filter=2}, --X
+    {name='Crushed Black Plutonium Ore', filter=2}, --X
+    {name='Crushed Adamantium Ore', filter=2}, --X
+
+    -- Filter 3: Wash --> Macerate --> Centrifuge
+    {name='Crushed Redstone Ore', filter=3},
+    {name='Crushed Red Fuchsite Ore', filter=3},
+    {name='Crushed Arsenopyrite Ore', filter=3},
+    {name='Crushed Fluor-Buergerite Ore', filter=3},
+    {name='Crushed Chromo-Alumino-Povondraite Ore', filter=3},
+    {name='Crushed Vanadio-Oxy-Dravite Ore', filter=3},
+    {name='Crushed Hedenbergite Ore', filter=3},
+    {name='Crushed Olenite Ore', filter=3},
+    {name='Crushed Bornite Ore', filter=3},
+    {name='Crushed Bauxite Ore', filter=3},
+    {name='Crushed Wittichenite Ore', filter=3},
+    {name='Crushed Pyrite Ore', filter=3},
+    {name='Ground Pitchblende', filter=3},
+    {name='Crushed Scheelite Ore', filter=3},
+    {name='Crushed Yellow Garnet Ore', filter=3},
+    {name='Crushed Green Fuchsite Ore', filter=3},
+    {name='Ground Glauconite Sand', filter=3},
+    {name='Crushed Uranium 238 Ore', filter=3},
+    {name='Crushed Electrotine Ore', filter=3},
+    {name='Crushed Zircon Ore', filter=3},
+    {name='Crushed Yttrialite Ore', filter=3},
+    {name='Crushed Florencite Ore', filter=3},
+    {name='Crushed Demicheleite (Br) Ore', filter=3},
+    {name='Crushed Lanthanite (Ce) Ore', filter=3},
+    {name='Crushed Agardite (Y) Ore', filter=3},
+    {name='Crushed Agardite (Cd) Ore', filter=3},
+    {name='Crushed Agardite (La) Ore', filter=3},
+    {name='Crushed Agardite (Nd) Ore', filter=3},
+    {name='Crushed Zimbabweite Ore', filter=3},
+    {name='Crushed Samarskite (Y) Ore', filter=3},
+    {name='Crushed Samarskite (Yb) Ore', filter=3},
+    {name='Crushed Bismutite Ore', filter=3}, --X
+    {name='Crushed Bismuth Ore', filter=3}, --X
+    {name='Ground Garnet Sand', filter=3}, --X
+    {name='Ground Cassiterite Sand', filter=3}, --X
+    {name='Crushed Gypsum Ore', filter=3}, --X
+    {name='Crushed Stibnite Ore', filter=3}, --X
+    {name='Crushed Saltpeter Ore', filter=3}, --X
+    {name='Crushed Chrome Ore', filter=3}, --X
+    {name='Crushed Silver Ore', filter=3}, --X
+    {name='Crushed Tungsten Ore', filter=3}, --X
+    {name='Crushed Antimony Ore', filter=3}, --X
+    {name='Crushed Arsenic Ore', filter=3}, --X
+    {name='Crushed Neodymium Ore', filter=3}, --X
+    {name='Crushed Cobaltite Ore', filter=3}, --X
+    {name='Crushed Ilmenite Ore', filter=3}, --X
+    {name='Crushed Roasted Iron Ore', filter=3}, --X
+    {name='Crushed Amber Ore', filter=3}, --X
+    {name='Crushed Yellow Limonite Ore', filter=3}, --X
+    {name='Crushed Brown Limonite Ore', filter=3}, --X
+    {name='Crushed Mica Ore', filter=3}, --X
+    {name='Crushed Molybdenum Ore', filter=3}, --X
+    {name='Crushed Vinteum Ore', filter=3}, --X
+    {name='Crushed Indium Ore', filter=3}, --X
+    {name='Crushed Wulfenite Ore', filter=3}, --X
+    {name='Crushed Powellite Ore', filter=3}, --X
+    {name='Crushed Molybdenite Ore', filter=3}, --X
+    {name='Crushed Tiberium Ore', filter=3}, --X
+    {name='Crushed Fluorspar Ore', filter=3}, --X
+    {name='Ground Basaltic Mineral Sand', filter=3}, --X
+    {name='Crushed Uvarovite Ore', filter=3}, --X
+    {name='Crushed Naquadria Ore', filter=3}, --X
+    {name='Crushed Opal Ore', filter=3}, --X
+    {name='Crushed Rubracium Ore', filter=3}, --X
+
+    -- Filter 4: Wash --> Sieve
+    {name='Crushed Lapis Ore', filter=4},
+    {name='Crushed Amethyst Ore', filter=4},
+    {name='Crushed Firestone Ore', filter=4},
+    {name='Crushed Cassiterite Ore', filter=4},
+    {name='Crushed Cinnabar Ore', filter=4},
+    {name='Crushed Certus Quartz Ore', filter=4},
+    {name='Crushed Quartzite Ore', filter=4},
+    {name='Crushed Emerald Ore', filter=4},
+    {name='Crushed Ordo Crystals', filter=4},
+    {name='Crushed Perditio Crystals', filter=4},
+    {name='Crushed Aer Crystals', filter=4},
+    {name='Crushed Ignis Crystals', filter=4},
+    {name='Crushed Terra Crystals', filter=4},
+    {name='Crushed Aqua Crystals', filter=4},
+    {name='Crushed Thorianite Ore', filter=4},
+    {name='Crushed Prasiolite Ore', filter=4},
+
+    -- Filter 5: Mercury Chemical Bath (Chemical Bath)
+    {name='Crushed Nickel Ore', filter=5},
+    {name='Crushed Galena Ore', filter=5},
+    {name='Crushed Meteoric Iron Ore', filter=5},
+    {name='Crushed Platinum Ore', filter=5},
+    {name='Crushed Sheldonite Ore', filter=5},
+    {name='Crushed Iridium Ore', filter=5},
+    {name='Crushed Osmium Ore', filter=5},
+
+    -- Filter 6: Sodium Persulfate Chemical Bath (Digester)
+    {name='Crushed Tantalite Ore', filter=6},
+    {name='Crushed Sphalerite Ore', filter=6},
+    
+    -- Filter 7: Digester (Trash)
+    {name='Crushed Monazite Ore', filter=7},
+    {name='Crushed Bastnasite Ore', filter=7},
+    {name='Crushed Lead Ore', filter=7},
+    {name='Crushed Fluorite (F) Ore', filter=7}
+
+    -- Filter 8: Other / Unlisted
+}
